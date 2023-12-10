3550e00aba4551e1de5ffb2f177d128cac6ad3a9
karenlee54 <124006475+karenlee54@users.noreply.github.com>
2023-12-10 00:19:14 -0800

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ point_list = [point]
 
 class Logo:
   x = 350
-  y = 440
+  y = 435
 
   def __init__(self):
     self.img1 = p5.loadImage('Logo/Logo.png')
@@ -30,10 +30,10 @@ class Logo:
     p5.push()
     p5.translate(self.x, self.y)
     p5.imageMode(p5.CENTER)
-    p5.image(self.img1, 0, 0, 300, 240)
+    p5.image(self.img1, 0, 0, 300, 260)
     p5.textAlign(p5.CENTER)
     p5.textFont(Font, 40)
-    p5.text('About Me', 0, 225)
+    p5.text('About Me', 0, 230)
     p5.textFont(Font, 25)
     p5.text('Click to start', 0, 550)
     p5.pop()
